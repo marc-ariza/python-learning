fb87244690217f10f7bc6b5d158d409152738f7d
marc <marcarizasarria@gmail.com>
2025-07-12 20:07:22 +0200

    im mega bad at this

--- Part3/Part3.4/Exercise7.py
+++ Part3/Part3.4/Exercise7.py
@@ -1,6 +1,18 @@
 def squared(word, number):
-    
+
+    count = 0
+    sentence = word * 1000000
+    result = 0
+
+    while True:
+        count += 1
+        if count <= number:
+            print(sentence[result:result+number])
+            result = result + number
+        else:
+            break
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    squared(ab, 3)
+    squared("abc", 5)
